8408c7fd3f9e5281d30a2eacdf654f15840b8e8c
Pero <seckovpero@gmail.com>
2018-03-21 13:49:55 +0100

    Added universal target (to enable framework to work with device and simulator )

---
+++ Output/TVizzyFramework-Debug-iphoneuniversal/TVizzyFramework.framework/Headers/TVizzyFramework.h
@@ -0,0 +1,20 @@
+//
+//  TVizzyFramework.h
+//  TVizzyFramework
+//
+//  Created by AspidaMacBook on 19/03/2018.
+//  Copyright © 2018 AspidaMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TVizzyFramework.
+FOUNDATION_EXPORT double TVizzyFrameworkVersionNumber;
+
+//! Project version string for TVizzyFramework.
+FOUNDATION_EXPORT const unsigned char TVizzyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+#import <TVizzyFramework/LoginView.h>
+#import <TVizzyFramework/NotificationVideoBL.h>
+
